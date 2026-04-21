9d6fdd94d601104d58e728715be3c6a9d9a0ec18
imahsoimah7522 <imahsoimah7522@gmail.com>
2026-04-21 17:18:14 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -7,7 +7,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 // ── INSTALLED CONFIG (auto-written by installer — do not edit manually) ──
-const INSTALLED_CONFIG = {"https://bvjucnttwxaoqwrsllvk.supabase.co":"","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2anVjbnR0d3hhb3F3cnNsbHZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NjI2MzEsImV4cCI6MjA5MjMzODYzMX0.mJBDk7D895PPe7izWaiifTVC1UydLuCpknIPwmlruho":"","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2anVjbnR0d3hhb3F3cnNsbHZrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3Njc2MjYzMSwiZXhwIjoyMDkyMzM4NjMxfQ.zhq5GqeeO46DmW4ymwvy5RWejBYfcvZoU6XFg0w_cXs":""};
+const INSTALLED_CONFIG = {"url":"https://wuzywjgtbgziomnmcwvj.supabase.co","anon_key":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1enl3amd0Ymd6aW9tbm1jd3ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NjYyNjYsImV4cCI6MjA5MjM0MjI2Nn0.LMmL_1ysFqZuzskuDjKT5sBTl3OMqHS3dCp5JDN738M","service_key":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1enl3amd0Ymd6aW9tbm1jd3ZqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3Njc2NjI2NiwiZXhwIjoyMDkyMzQyMjY2fQ.h2wdo8142iMqFX5eEYGVDFc2o-zIPrXjgA5bMmK7Gek"};
 // ── END INSTALLED CONFIG ──
 
 // Read config from localStorage (set by the CMS installer)
